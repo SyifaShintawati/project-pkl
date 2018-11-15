6fbe7a713bef0e0b0f8740f4a1ea61bbc708e37a
SyifaShintawati <syifasintawatinurazizah@gmail.com>
2018-11-15 15:21:06 +0700

    baru

--- resources/views/tambih.blade.php
+++ resources/views/tambih.blade.php
@@ -108,7 +108,7 @@
                          <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Tambah') }}
+                                    {{ __('Add') }}
                                 </button>
                             </div>
                         </div>
